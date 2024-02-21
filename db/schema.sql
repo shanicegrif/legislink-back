1f320ba14006321b2122d13a2952e3753d00a2c3
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2024-02-20 21:04:26 -0500

    adds basic schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS legislink_db_dev;
+CREATE DATABASE legislink_db_dev;
+
+\c legislink_db_dev;
+
+/*
+*   use underscore_name for db, do not change them to camelCase.
+*   table names should b plural
+*/
+
+/*
+    TODO: use COMMENT when we have time.
+        need more tables to store other data.
+*/
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(40) UNIQUE NOT NULL,
+    user_password VARCHAR(40) NOT NULL,
+    user_zipcode INTEGER NOT NULL,
+    manager BOOLEAN
+);
