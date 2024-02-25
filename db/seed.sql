f48a11bad4c614ad525224c31b671f542a6594a5
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2024-02-25 15:04:36 -0500

    updateseed for user table

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+\c \c movies_dev;
+
+INSERT INTO users (username, email, password_hash)
+VALUES
+('user1', 'user1@example.com', 'hashed_password1'),
+('user2', 'user2@example.com', 'hashed_password2');
